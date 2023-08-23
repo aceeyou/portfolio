60177293679aec2f9205599f46662f7eae222252
Ace Logronio <64186285+aceeyou@users.noreply.github.com>
2023-08-23 15:23:10 +0800

    mobile first design width 390px

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const navOpenBtn = document.querySelector("#menu-mobile");
+const navCloseBtn = document.querySelector("#menu-mobile-close");
+const navMobieMenuOpen = document.querySelector(".overlay__mobile_nav_open");
+const navContainer = document.querySelector(".mobile__nav");
+
+let isNavOpen = false;
+navOpenBtn.addEventListener("click", () => {
+  isNavOpen = true;
+  navContainer.classList.toggle("mobile__nav-close");
+  navMobieMenuOpen.style.display = "block";
+});
+
+navCloseBtn.addEventListener("click", () => {
+  isNavOpen = false;
+  navContainer.classList.toggle("mobile__nav-close");
+  navMobieMenuOpen.style.display = "none";
+});
